ac913182e36f89a5396babac370e69eaf5c07be9
MostafaBelala <mostafabelala15@gmail.com>
2025-12-10 22:19:25 +0200

    trigers added

--- Database/Mr. Librarian.sql
+++ Database/Mr. Librarian.sql
@@ -1,15 +1,14 @@
-
-### member
+--
 -- CREATE TABLE member (
---     member_id int PRIMARY KEY AUTO_INCREMENT,
+--     member_id VARCHAR(10) PRIMARY KEY,
 --     member_name VARCHAR(100),
 --     phone VARCHAR(20) UNIQUE,
 --     email VARCHAR(100) UNIQUE
 -- );
 
-### book
+-- 
 -- CREATE TABLE book (
---     book_id int PRIMARY KEY AUTO_INCREMENT,
+--     book_id VARCHAR(10) PRIMARY KEY,
 --     title VARCHAR(200),
 --     author VARCHAR(100),
 --     publisher VARCHAR(100),
@@ -18,13 +17,13 @@
 --     available_copies INT
 -- );
 
-### borrow
+-- 
 -- CREATE TABLE borrow (
---     borrow_id int PRIMARY KEY AUTO_INCREMENT,
---     member_id int,
---     borrow_date DATE default (CURRENT_DATE),
---     due_date DATE default (CURRENT_DATE + interval 2 month),
---     book_id int,
+--     borrow_id VARCHAR(10) PRIMARY KEY,
+--     member_id VARCHAR(10),
+--     borrow_date DATE DEFAULT (CURRENT_DATE),
+--     due_date DATE DEFAULT (CURRENT_DATE + INTERVAL 2 MONTH),
+--     book_id VARCHAR(10),
 
 --     CONSTRAINT fk_borrow_member
 --         FOREIGN KEY (member_id) REFERENCES member(member_id),
@@ -33,22 +32,98 @@
 --         FOREIGN KEY (book_id) REFERENCES book(book_id)
 -- );
 
-### Return
+-- 
 -- CREATE TABLE return_book (
---     return_id int PRIMARY KEY AUTO_INCREMENT,
---     borrow_id int,
---     return_date DATE default (CURRENT_DATE),
+--     return_id VARCHAR(10) PRIMARY KEY,
+--     borrow_id VARCHAR(10),
+--     return_date DATE DEFAULT (CURRENT_DATE),
 --     fine_amount DECIMAL(10,2),
 
 --     CONSTRAINT fk_return_borrow
 --         FOREIGN KEY (borrow_id) REFERENCES borrow(borrow_id)
 -- );
 
-### user validation
+--
 -- CREATE TABLE user_validation (
 --     username VARCHAR(50) NOT NULL,
 --     password VARCHAR(255) NOT NULL
 -- );
-select * from borrow;
+
+### Default IDs:
+-- DELIMITER $$
+
+-- CREATE TRIGGER trg_member_id
+-- BEFORE INSERT ON member
+-- FOR EACH ROW
+-- BEGIN
+--     DECLARE last_id INT;
+
+--     SELECT IFNULL(MAX(CAST(SUBSTRING(member_id, 2) AS UNSIGNED)), 0)
+--     INTO last_id
+--     FROM member;
+
+--     SET NEW.member_id = CONCAT('M', LPAD(last_id + 1, 3, '0'));
+-- END$$
+
+-- DELIMITER ;
+
+-- DELIMITER $$
+
+-- CREATE TRIGGER trg_book_id
+-- BEFORE INSERT ON book
+-- FOR EACH ROW
+-- BEGIN
+--     DECLARE last_id INT;
+
+--     SELECT IFNULL(MAX(CAST(SUBSTRING(book_id, 2) AS UNSIGNED)), 0)
+--     INTO last_id
+--     FROM book;
+
+--     SET NEW.book_id = CONCAT('B', LPAD(last_id + 1, 3, '0'));
+-- END$$
+
+-- DELIMITER ;
+
+-- DELIMITER $$
+
+-- CREATE TRIGGER trg_borrow_id
+-- BEFORE INSERT ON borrow
+-- FOR EACH ROW
+-- BEGIN
+--     DECLARE last_id INT;
+
+--     SELECT IFNULL(MAX(CAST(SUBSTRING(borrow_id, 3) AS UNSIGNED)), 0)
+--     INTO last_id
+--     FROM borrow;
+
+--     SET NEW.borrow_id = CONCAT('BR', LPAD(last_id + 1, 3, '0'));
+-- END$$
+
+-- DELIMITER ;
+
+-- DELIMITER $$
+
+-- CREATE TRIGGER trg_return_id
+-- BEFORE INSERT ON return_book
+-- FOR EACH ROW
+-- BEGIN
+--     DECLARE last_id INT;
+
+--     SELECT IFNULL(MAX(CAST(SUBSTRING(return_id, 2) AS UNSIGNED)), 0)
+--     INTO last_id
+--     FROM return_book;
+
+--     SET NEW.return_id = CONCAT('R', LPAD(last_id + 1, 3, '0'));
+-- END$$
+
+-- DELIMITER ;
+
+INSERT INTO member(member_name, phone, email)
+VALUES 
+('Ali', '01012345678', 'ali@gmail.com'),
+('Sara', '01087654321', 'sara@gmail.com');
+
+
+select * from member;
 
 
